18b9910561efa9c8af47da03e19ddd341c055812
LanNguyen135 <lannguyen13595@gmail.com>
2023-10-14 21:24:52 -0500

    ETL completed

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Project3
+
+Data source: https://www.kaggle.com/code/spittman1248/effect-of-socioeconomic-status-on-obesity/input
---
+++ gunViolence.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS gunViolence;
+
+CREATE TABLE gunViolence (
+	incident_id INT PRIMARY KEY,
+	date DATE,
+	state VARCHAR(15),
+	state_code VARCHAR(2),
+	city_or_county VARCHAR(50),
+	n_killed INT,
+	n_injured INT,
+	population_2020 INT
+);
